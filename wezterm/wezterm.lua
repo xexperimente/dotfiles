8b1fe3272225d1a0fd7933fa73e319e9e38b1673
Petr Kadlec <xexperimente@gmail.com>
2023-11-02 13:58:34 +0100

    [wezterm] initial config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,58 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+local colors = require("lua/rose-pine-dawn").colors()
+local window_frame = require("lua/rose-pine-dawn").window_frame()
+
+config.colors = colors
+config.window_frame = window_frame
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'AdventureTime'
+config.win32_system_backdrop = "Auto" --'Acrylic'
+config.window_background_opacity = 0.95
+
+config.font = wezterm.font("Delugia")
+
+config.initial_rows = 45
+config.initial_cols = 165
+
+config.default_prog = { "pwsh" }
+config.window_decorations = "RESIZE"
+
+-- Windows default paste from clipboard
+config.keys = {
+	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+}
+
+-- Copy on select
+config.mouse_bindings = {
+	-- Change the default click behavior so that it only selects
+	-- text and doesn't open hyperlinks, and that it populates
+	-- the Clipboard rather the PrimarySelection which is part
+	-- of the default assignment for a left mouse click.
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.CompleteSelection("Clipboard"),
+	},
+	{
+		event = { Up = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = act({ PasteFrom = "Clipboard" }),
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
