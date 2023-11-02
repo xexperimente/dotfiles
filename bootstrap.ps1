e2313896c0e7b7ae3359d517124e394f448e010c
Petr Kadlec <xexperimente@gmail.com>
2023-11-02 14:19:24 +0100

    [Install script] wezterm instalation

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -6,7 +6,7 @@
 .PARAMETER Force
     Overwrite existing symlinks or files. Default is False.
 .PARAMETER Backup
-    Rename existing files to .BAK. By default.
+    Rename existing file to .BAK. Only for files, not folders. Default is False.
 .EXAMPLE
     .\bootstrap.ps1 -Force -Backup
 #>
@@ -73,11 +73,68 @@ function Install-File
 	Write-Host "  Done`n" -ForegroundColor Green
 }
 
+# Install folder
+function Install-Folder
+{
+	param (
+		[Parameter(Mandatory)]
+		[string]$Name,
+
+		[Parameter(Mandatory)]
+		[string]$Target,
+		
+		[Parameter(Mandatory)]
+		[string]$Source,
+
+		[Parameter()]
+		[Switch]$Force = $False
+	)
+
+	Write-Host "$($Name):" -ForegroundColor Blue
+
+	$symlinkPresent = $false;
+	if (Test-Path $Target)
+	{
+		if ((Get-item $Target).LinkType -eq "SymbolicLink")
+		{
+			Write-Host "  Symlink present"
+			$symlinkPresent = $true
+		} elseif ($Force)
+		{
+			Write-Host "  Target present and -Force, removing"
+			Remove-Item $Target -Recurse -Confirm
+		}
+	}
+
+	if ((-not $symlinkPresent) -or $Force)
+	{
+		$err = @{}
+
+		Write-Host "  Creating symlink"
+		New-Item -ItemType SymbolicLink -Path $Target -Value $Source -Force:$Force -EA SilentlyContinue -EV err | Out-Null
+
+		if ($err)
+		{
+			Write-Host "  Error: $($err) " -ForegroundColor Red 
+		}
+
+	}
+
+	Write-Host "  Done`n" -ForegroundColor Green
+}
+
 # Install each dotfiles
 
 Install-File -Name "Powershell" -Target $profile -Source "$env:USERPROFILE/Dotfiles/Powershell/Microsoft.PowerShell_profile.ps1" -Backup:$Backup -Force:$Force
 
 Install-File -Name "Starship" -Target "$env:USERPROFILE/.config/starship.toml" -Source "$env:USERPROFILE/Dotfiles/Starship/starship.toml" -Backup:$Backup -Force:$Force
+
+Install-Folder -Name "wezterm" -Target "$env:USERPROFILE/.config/wezterm" -Source "$env:USERPROFILE/Dotfiles/wezterm" -Force:$Force
+
+
+
+
+
 
 
 
