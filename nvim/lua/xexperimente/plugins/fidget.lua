bc7e5a2416056bdb2b772410b611c00fc70e2e86
Petr Kadlec <xexperimente@gmail.com>
2023-11-15 09:12:34 +0100

    [Neovim] fidget options

--- nvim/lua/xexperimente/plugins/fidget.lua
+++ nvim/lua/xexperimente/plugins/fidget.lua
@@ -1,5 +1,15 @@
 local Plugin = { 'j-hui/fidget.nvim' }
 
+Plugin.opts = {
+	progress = {
+		ignore_done_already = true,
+		display = {
+			render_limit = 10,
+			done_icon = 'done',
+		},
+	},
+}
+
 Plugin.config = true
 
 return Plugin
