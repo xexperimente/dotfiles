1163b9544712c7e6d209e71288c40132bc22f5ea
Petr Kadlec <xexperimente@gmail.com>
2024-06-03 09:47:10 +0200

    [Neovim] updated configuration of lazydev.nvim

--- nvim/lua/specs/lazydev.lua
+++ nvim/lua/specs/lazydev.lua
@@ -4,7 +4,7 @@ Plugin.ft = 'lua'
 
 Plugin.opts = {
 	library = {
-		vim.env.LAZY .. '/luvit-meta/library', -- see below
+		'luvit-meta/library',
 		-- You can also add plugins you always want to have loaded.
 		-- Useful if the plugin has globals or types you want to use
 		-- vim.env.LAZY .. "/LazyVim", -- see below
