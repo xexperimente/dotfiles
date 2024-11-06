c355e8e76d516b22a67be3d98f9b916f171959ba
Petr Kadlec <xexperimente@gmail.com>
2024-11-06 08:36:34 +0100

    [Powershell] homeoofice backup

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -96,6 +96,7 @@ function Backup-Starship
 function Backup-Homeoffice
 {
 	& rclone sync C:\_Projects\KVP\src E:\_Projects_svn\KVP\src --exclude-from="$env:USERPROFILE\kovoprog.rclone.exclude" $args -P
+	& rclone sync C:\_Projects\KVP\data E:\_Projects_svn\KVP\data $args -P
 }
 
 function Backup-Sources
