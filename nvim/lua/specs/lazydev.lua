c38e71d3bfc86290b60b0f592fbe6e06b07d25d3
Petr Kadlec <xexperimente@gmail.com>
2025-01-21 12:56:55 +0100

    [Neovim] add global Snacks to lazydev

--- nvim/lua/specs/lazydev.lua
+++ nvim/lua/specs/lazydev.lua
@@ -5,6 +5,9 @@ Plugin.ft = 'lua'
 Plugin.opts = {
 	library = {
 		'luvit-meta/library',
+		{ path = 'snacks.nvim', words = { 'Snacks' } },
+		{ path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+
 		-- You can also add plugins you always want to have loaded.
 		-- Useful if the plugin has globals or types you want to use
 		-- vim.env.LAZY .. "/LazyVim", -- see below
