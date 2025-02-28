3fdb2749ba02b7eb90839d50808a70cc384f8cb3
Petr Kadlec <xexperimente@gmail.com>
2025-02-28 10:04:14 +0100

    [Neovim] add minuet-ai suggestions with gemini backend

---
+++ nvim/lua/specs/ai.lua
@@ -0,0 +1,20 @@
+local Plugin = { 'milanglacier/minuet-ai.nvim' }
+
+Plugin.opts = {
+	virtualtext = {
+		keymap = {
+			accept = '<A-y>',
+			accept_line = '<A-Y>',
+			dismiss = '<A-e>',
+			prev = '<A-[>',
+			next = '<A-]>',
+		},
+	},
+	provider = 'gemini',
+}
+
+function Plugin.init()
+	vim.keymap.set('n', '<leader>ua', '<cmd>Minuet virtualtext toggle<cr>', { desc = 'Toggle AI suggestions' })
+end
+
+return Plugin
