c392e287e21f05650fcf92830448333da0341388
Petr Kadlec <xexperimente@gmail.com>
2025-04-04 19:58:06 +0200

    [Neovim] look and feel

--- nvim/lua/xexperimente/core/lsp.lua
+++ nvim/lua/xexperimente/core/lsp.lua
@@ -1,4 +1,19 @@
 vim.lsp.enable({
-	"lua-ls",
-	"zls"
+	'lua-ls',
+	'zls',
+})
+
+vim.diagnostic.config({
+	update_in_insert = false,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = '',
+			[vim.diagnostic.severity.WARN] = '',
+			[vim.diagnostic.severity.HINT] = '',
+			[vim.diagnostic.severity.INFO] = '',
+		},
+	},
+	float = {
+		border = 'single',
+	},
 })
