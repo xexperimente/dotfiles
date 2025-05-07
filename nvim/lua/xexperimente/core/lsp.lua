1a3ac3a8e431649c2e64d9f548b2c35930dbd6aa
Petr Kadlec <xexperimente@gmail.com>
2025-05-07 17:21:54 +0200

    [Neovim] lsp icons

--- nvim/lua/xexperimente/core/lsp.lua
+++ nvim/lua/xexperimente/core/lsp.lua
@@ -7,10 +7,10 @@ vim.diagnostic.config({
 	update_in_insert = false,
 	signs = {
 		text = {
-			[vim.diagnostic.severity.ERROR] = '',
+			[vim.diagnostic.severity.ERROR] = '',
 			[vim.diagnostic.severity.WARN] = '',
-			[vim.diagnostic.severity.HINT] = '',
-			[vim.diagnostic.severity.INFO] = '',
+			[vim.diagnostic.severity.HINT] = '󰌵',
+			[vim.diagnostic.severity.INFO] = '',
 		},
 	},
 	float = {
