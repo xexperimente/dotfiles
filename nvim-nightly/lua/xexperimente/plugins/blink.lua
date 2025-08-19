85e9c7d60b6803cb577eff7f9f5d687b6291cb20
Petr Kadlec <xexperimente@gmail.com>
2025-08-19 09:18:58 +0200

    [Neovim] blink.cmp release version

--- nvim-nightly/lua/xexperimente/plugins/blink.lua
+++ nvim-nightly/lua/xexperimente/plugins/blink.lua
@@ -1,5 +1,8 @@
 vim.pack.add({
-	{ src = 'https://github.com/saghen/blink.cmp', version = 'main' },
+	{ 
+		src = 'https://github.com/saghen/blink.cmp', 
+		version = vim.version.range('1.*') 
+	},
 })
 
 require('blink.cmp').setup({
