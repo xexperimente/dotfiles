1adb66d6212486aa5e4dc6f6520d6ad54dfd6ce3
Petr Kadlec <xexperimente@gmail.com>
2025-08-28 13:57:00 +0200

    [Neovim] nightly: cleanup

--- nvim-nightly/lua/xexperimente/plugins/snacks.lua
+++ nvim-nightly/lua/xexperimente/plugins/snacks.lua
@@ -203,49 +203,46 @@ Snacks.toggle.line_number():map('<leader>ul')
 Snacks.toggle.inlay_hints():map('<leader>uh')
 Snacks.toggle.dim():map('<leader>uD')
 Snacks.toggle.treesitter():map('<leader>uT')
--- Snacks.toggle.indent():map('<leader>ug')
--- Snacks.toggle.profiler():map('<leader>up')
--- Snacks.toggle.profiler_highlights():map('<leader>uP')
 
 -- Utils
-bind('n', '<leader>bd', '<cmd>lua Snacks.bufdelete()<cr>', { desc = 'Delete buffer' })
-bind('n', '<leader>bD', '<cmd>lua Snacks.bufdelete.other()<cr>', { desc = 'Delete other buffers' })
+bind('n', '<leader>bd', function() Snacks.bufdelete() end, { desc = 'Delete buffer' })
+bind('n', '<leader>bD', function() Snacks.bufdelete.other() end, { desc = 'Delete other buffers' })
 
 -- Pickers
-bind('n', '<leader>ff', '<cmd>lua Snacks.picker.files()<cr>', { desc = 'Files' })
-bind('n', '<leader>fe', '<cmd>lua Snacks.explorer()<cr>', { desc = 'Explorer' })
-bind('n', '<leader>fc', '<cmd>lua Snacks.picker.highlights()<cr>', { desc = 'Highlight groups' })
-bind('n', '<leader>fb', '<cmd>lua Snacks.picker.buffers()<cr>', { desc = 'Buffers' })
-bind('n', '<leader>fh', '<cmd>lua Snacks.picker.help()<cr>', { desc = 'Help' })
-bind('n', '<leader>fk', '<cmd>lua Snacks.picker.keymaps()<cr>', { desc = 'Keymaps' })
-bind('n', '<leader>fu', '<cmd>lua Snacks.picker.undo()<cr>', { desc = 'Undo' })
-bind('n', '<leader>fg', '<cmd>lua Snacks.picker.grep()<cr>', { desc = 'Grep' })
-bind('n', '<leader>fG', '<cmd>lua Snacks.picker.grep_word()<cr>', { desc = 'Grep word' })
-bind('n', '<leader>fm', '<cmd>lua Snacks.picker.marks()<cr>', { desc = 'Marks' })
-bind('n', '<leader>fi', '<cmd>lua Snacks.picker.icons()<cr>', { desc = 'Icons' })
-bind('n', '<leader>fq', '<cmd>lua Snacks.picker.qflist()<cr>', { desc = 'Quickfix list' })
-bind('n', '<leader>fl', '<cmd>lua Snacks.picker.loclist()<cr>', { desc = 'Location list' })
+bind('n', '<leader>ff', function() Snacks.picker.files() end, { desc = 'Files' })
+bind('n', '<leader>fe', function() Snacks.explorer() end, { desc = 'Explorer' })
+bind('n', '<leader>fc', function() Snacks.picker.highlights() end, { desc = 'Highlight groups' })
+bind('n', '<leader>fb', function() Snacks.picker.buffers() end, { desc = 'Buffers' })
+bind('n', '<leader>fh', function() Snacks.picker.help() end, { desc = 'Help' })
+bind('n', '<leader>fk', function() Snacks.picker.keymaps() end, { desc = 'Keymaps' })
+bind('n', '<leader>fu', function() Snacks.picker.undo() end, { desc = 'Undo' })
+bind('n', '<leader>fg', function() Snacks.picker.grep() end, { desc = 'Grep' })
+bind('n', '<leader>fG', function() Snacks.picker.grep_word() end, { desc = 'Grep word' })
+bind('n', '<leader>fm', function() Snacks.picker.marks() end, { desc = 'Marks' })
+bind('n', '<leader>fi', function() Snacks.picker.icons() end, { desc = 'Icons' })
+bind('n', '<leader>fq', function() Snacks.picker.qflist() end, { desc = 'Quickfix list' })
+bind('n', '<leader>fl', function() Snacks.picker.loclist() end, { desc = 'Location list' })
 
 -- Git
-bind('n', '<leader>gl', '<cmd>lua Snacks.picker.git_log()<cr>', { desc = 'Git commits' })
-bind('n', '<leader>gb', '<cmd>lua Snacks.picker.git_branches()<cr>', { desc = 'Git branches' })
-bind('n', '<leader>gs', '<cmd>lua Snacks.picker.git_status()<cr>', { desc = 'Git status' })
-bind('n', '<leader>gB', '<cmd>lua Snacks.git.blame_line({count=1})<cr>', { desc = 'Git Blame Line' })
+bind('n', '<leader>gl', function() Snacks.picker.git_log() end, { desc = 'Git commits' })
+bind('n', '<leader>gb', function() Snacks.picker.git_branches() end, { desc = 'Git branches' })
+bind('n', '<leader>gs', function() Snacks.picker.git_status() end, { desc = 'Git status' })
+bind('n', '<leader>gB', function() Snacks.git.blame_line({ count = 1 }) end, { desc = 'Git Blame Line' })
 
 -- Diagnostics
-bind('n', '<leader>lp', '<cmd>lua Snacks.picker.diagnostics_buffer()<cr>', { desc = 'Show buffer diagnostics' })
-bind('n', '<leader>lP', '<cmd>lua Snacks.picker.diagnostics()<cr>', { desc = 'Show all diagnostics' })
+bind('n', '<leader>lp', function() Snacks.picker.diagnostics_buffer() end, { desc = 'Show buffer diagnostics' })
+bind('n', '<leader>lP', function() Snacks.picker.diagnostics() end, { desc = 'Show all diagnostics' })
 
 -- Terminal
-bind('n', '<leader>t', '<cmd>lua Snacks.terminal.toggle()<cr>', { desc = 'Toggle terminal' })
-bind('n', '<c-t>', '<cmd>lua Snacks.terminal.toggle()<cr>', { desc = 'Toggle terminal' })
+bind('n', '<leader>t', function() Snacks.terminal.toggle() end, { desc = 'Toggle terminal' })
+bind('n', '<c-t>', function() Snacks.terminal.toggle() end, { desc = 'Toggle terminal' })
 
 -- Zen mode
-bind('n', '<leader>z', '<cmd>lua Snacks.zen()<cr>', { desc = 'Toggle Zen Mode' })
-bind('n', '<leader>Z', '<cmd>lua Snacks.zen.zoom()<cr>', { desc = 'Toggle Zoom' })
+bind('n', '<leader>z', function() Snacks.zen() end, { desc = 'Toggle Zen Mode' })
+bind('n', '<leader>Z', function() Snacks.zen.zoom() end, { desc = 'Toggle Zoom' })
 
 -- Notification history
-bind('n', '<leader>n', '<cmd>lua Snacks.notifier.show_history()<cr>', { desc = 'Show Notification History' })
+bind('n', '<leader>n', function() Snacks.notifier.show_history() end, { desc = 'Show Notification History' })
 bind('n', '<leader>m', function()
 	local messages = vim.api.nvim_exec2('messages', { output = true })
 
@@ -292,8 +289,8 @@ bind(
 )
 
 -- Scratch buffer
-bind('n', '<leader>.', '<cmd>lua Snacks.scratch()<cr>', { desc = 'Toggle Scratch Buffer' })
-bind('n', '<leader>;', '<cmd>lua Snacks.scratch.select()<cr>', { desc = 'Select Scratch Buffer' })
+bind('n', '<leader>.', function() Snacks.scratch() end, { desc = 'Toggle Scratch Buffer' })
+bind('n', '<leader>;', function() Snacks.scratch.select() end, { desc = 'Select Scratch Buffer' })
 
 -- https://github.com/folke/snacks.nvim/blob/main/docs/notifier.md
 vim.api.nvim_create_autocmd('LspProgress', {
