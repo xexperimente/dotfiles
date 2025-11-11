ff55a5f164a5969a02187c7bf7c0d2d76c485e6a
Petr Kadlec <xexperimente@gmail.com>
2025-11-11 11:46:56 +0100

    [Neovim] nightly: treesitter and emmylua updates

--- nvim-nightly/lsp/emmylua_ls.lua
+++ nvim-nightly/lsp/emmylua_ls.lua
@@ -1,4 +1,15 @@
-return {
+--- Get lib paths for input packages.
+--- @param pkgs string[]
+local function libs(pkgs)
+	for p, pkg in ipairs(pkgs) do
+		pkgs[p] = vim.fn.stdpath('data') .. '/site/pack/core/opt/' .. pkg
+	end
+	return pkgs
+end
+
+--- @type vim.lsp.Config
+local result = {
+
 	cmd = { 'emmylua_ls' },
 	filetypes = { 'lua' },
 	root_markers = {
@@ -14,6 +25,7 @@ return {
 				globals = {
 					'vim',
 					'jit',
+					'Snacks',
 				},
 				disable = {},
 			},
@@ -21,10 +33,24 @@ return {
 				version = 'LuaJIT',
 			},
 			workspace = {
-				library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
-					vim.fn.stdpath('data') .. '/site/pack/core/opt/',
-				}),
+				-- library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
+				-- 	vim.fn.stdpath('data') .. '/site/pack/core/opt/',
+				-- }),
+				library = {
+					vim.env.VIMRUNTIME .. '/lua',
+					vim.env.VIMRUNTIME .. '/plugin',
+					unpack(libs({
+						'flash.nvim',
+						'mini.nvim',
+						'nvim-treesitter',
+						'nvim-treesitter-textobjects',
+						'witch-line',
+						'snacks.nvim',
+					})),
+				},
 			},
 		},
 	},
 }
+
+return result
--- nvim-nightly/lua/xexperimente/plugins/treesitter.lua
+++ nvim-nightly/lua/xexperimente/plugins/treesitter.lua
@@ -1,28 +1,23 @@
+local function gh(pkg) return { src = 'https://github.com/' .. pkg, version = 'main' } end
+
 vim.pack.add({
-	{
-		src = 'https://github.com/nvim-treesitter/nvim-treesitter',
-		version = 'master',
-	},
+	gh('nvim-treesitter/nvim-treesitter'),
+	gh('nvim-treesitter/nvim-treesitter-textobjects'),
 })
 
-require('nvim-treesitter.configs').setup({
-	ensure_installed = {
-		'bash',
-		'lua',
-		'rust',
-		'regex',
-		'vim',
-		'vimdoc',
-		'markdown',
-		'markdown_inline',
-	},
-	auto_install = false,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = true,
+local ts = require('nvim-treesitter')
+
+ts.install({ 'bash', 'rust', 'regex', 'lua', 'vim', 'vimdoc', 'markdown', 'markdown_inline', 'zig', 'nu' })
+
+require('nvim-treesitter-textobjects').setup({
+	select = {
+		lookahead = true,
+		selection_modes = {
+			['@parameter.outer'] = 'v', -- charwise
+			['@function.outer'] = 'V', -- linewise
+			['@class.outer'] = '<c-v>', -- blockwise
+		},
+		include_surrounding_whitespace = false,
 	},
 })
 
@@ -31,14 +26,19 @@ vim.api.nvim_create_autocmd('PackChanged', {
 	group = vim.api.nvim_create_augroup('nvim-treesitter-pack-changed-update-handler', { clear = true }),
 	callback = function(event)
 		if event.data.kind == 'update' and event.data.spec.name == 'nvim-treesitter' then
-			vim.notify('nvim-treesitter updated, running TSUpdate...', vim.log.levels.INFO)
-			---@diagnostic disable-next-line: param-type-not-match
-			local ok = pcall(vim.cmd, 'TSUpdate')
-			if ok then
-				vim.notify('TSUpdate completed successfully!', vim.log.levels.INFO)
-			else
-				vim.notify('TSUpdate command not available yet, skipping', vim.log.levels.WARN)
-			end
+			vim.notify('nvim-treesitter: updating languages', vim.log.levels.INFO)
+			ts.update()
 		end
 	end,
 })
+
+local sel = require('nvim-treesitter-textobjects.select')
+local swap = require('nvim-treesitter-textobjects.swap')
+
+vim.keymap.set({ 'x', 'o' }, 'af', function() sel.select_textobject('@function.outer', 'textobjects') end)
+vim.keymap.set({ 'x', 'o' }, 'if', function() sel.select_textobject('@function.inner', 'textobjects') end)
+vim.keymap.set({ 'x', 'o' }, 'ac', function() sel.select_textobject('@class.outer', 'textobjects') end)
+vim.keymap.set({ 'x', 'o' }, 'ic', function() sel.select_textobject('@class.inner', 'textobjects') end)
+vim.keymap.set({ 'x', 'o' }, 'as', function() sel.select_textobject('@local.scope', 'locals') end)
+vim.keymap.set('n', '<leader>x', function() swap.swap_next('@parameter.inner') end)
+vim.keymap.set('n', '<leader>X', function() swap.swap_previous('@parameter.outer') end)
