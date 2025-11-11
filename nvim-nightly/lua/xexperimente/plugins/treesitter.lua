34944cdc4ea0fc4d3d8f9fa251667ab023658414
Petr Kadlec <xexperimente@gmail.com>
2025-11-11 13:40:18 +0100

    [Neovim] nightly: autoinstall ts parsers

--- nvim-nightly/lua/xexperimente/plugins/treesitter.lua
+++ nvim-nightly/lua/xexperimente/plugins/treesitter.lua
@@ -7,7 +7,7 @@ vim.pack.add({
 
 local ts = require('nvim-treesitter')
 
-ts.install({ 'bash', 'rust', 'regex', 'lua', 'vim', 'vimdoc', 'markdown', 'markdown_inline', 'zig', 'nu' })
+ts.install({ 'regex', 'lua', 'vim', 'vimdoc', 'markdown', 'markdown_inline' })
 
 require('nvim-treesitter-textobjects').setup({
 	select = {
@@ -21,17 +21,6 @@ require('nvim-treesitter-textobjects').setup({
 	},
 })
 
-vim.api.nvim_create_autocmd('PackChanged', {
-	desc = 'Handle nvim-treesitter updates',
-	group = vim.api.nvim_create_augroup('nvim-treesitter-pack-changed-update-handler', { clear = true }),
-	callback = function(event)
-		if event.data.kind == 'update' and event.data.spec.name == 'nvim-treesitter' then
-			vim.notify('nvim-treesitter: updating languages', vim.log.levels.INFO)
-			ts.update()
-		end
-	end,
-})
-
 local sel = require('nvim-treesitter-textobjects.select')
 local swap = require('nvim-treesitter-textobjects.swap')
 
@@ -42,3 +31,46 @@ vim.keymap.set({ 'x', 'o' }, 'ic', function() sel.select_textobject('@class.inne
 vim.keymap.set({ 'x', 'o' }, 'as', function() sel.select_textobject('@local.scope', 'locals') end)
 vim.keymap.set('n', '<leader>x', function() swap.swap_next('@parameter.inner') end)
 vim.keymap.set('n', '<leader>X', function() swap.swap_previous('@parameter.outer') end)
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup('TreesitterCommands', { clear = true })
+
+autocmd('PackChanged', {
+	group = augroup,
+	pattern = { 'nvim-treesitter' },
+	callback = function(_event)
+		-- if event.data.kind == 'update' and event.data.spec.name == 'nvim-treesitter' then
+		vim.notify('Updating treesitter parsers', vim.log.levels.INFO)
+		ts.update(nil, { summary = true })
+		-- end
+	end,
+})
+
+autocmd('FileType', {
+	group = augroup,
+	pattern = { '*' },
+	callback = function(event)
+		local filetype = event.match
+		local lang = vim.treesitter.language.get_lang(filetype)
+		local is_installed, _error = vim.treesitter.language.add(lang)
+
+		if not is_installed then
+			local available_langs = ts.get_available()
+			local is_available = vim.tbl_contains(available_langs, lang)
+
+			-- TODO: confirmation dialog
+
+			if is_available then
+				vim.notify('Installing treesitter parser for ' .. lang, vim.log.levels.INFO)
+				ts.install({ lang }):wait(30 * 1000)
+			end
+		end
+
+		local ok, _ = pcall(vim.treesitter.start, event.buf, lang)
+		if not ok then return end
+
+		--
+		-- vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		-- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+	end,
+})
