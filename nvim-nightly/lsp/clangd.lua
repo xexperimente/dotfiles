f632464c9e5c4fcdb92883371313fc458fe2af9f
Petr Kadlec <xexperimente@gmail.com>
2026-02-05 14:31:17 +0100

    [Neovim] nightly: clangd switch_source_header

--- nvim-nightly/lsp/clangd.lua
+++ nvim-nightly/lsp/clangd.lua
@@ -1,9 +1,21 @@
+--- Implements the off-spec textDocument/switchSourceHeader method.
+--- @param buf integer
+local function switch_source_header(client, buf)
+	client:request('textDocument/switchSourceHeader', vim.lsp.util.make_text_document_params(buf), function(err, result)
+		if err then
+			vim.notify(err.message, vim.log.levels.ERROR)
+			return
+		end
+		if not result then
+			vim.notify('Corresponding file could not be determined', vim.log.levels.WARN)
+			return
+		end
+		vim.cmd.edit(vim.uri_to_fname(result))
+	end)
+end
+
 return {
-	cmd = {
-		jit.os:find('Windows')
-				and 'C:/Program Files/Microsoft Visual Studio/18/Professional/VC/Tools/Llvm/x64/bin/clangd.exe'
-			or 'clangd',
-	},
+	cmd = { 'clangd' },
 	filetypes = { 'c', 'cpp', 'h', 'hpp', 'ixx' },
 	root_markers = {
 		'.clangd',
@@ -11,7 +23,6 @@ return {
 		'.clang-format',
 		'compile_commands.json',
 		'compile_flags.txt',
-		-- 'configure.ac', -- GNU Autotools.
 	},
 	reuse_client = function(client, config) return client.name == config.name end,
 	settings = {
@@ -38,28 +49,31 @@ return {
 	end,
 
 	-- Assumes at most one clangd client is attached to a buffer.
-	on_attach = function(_client, _buf)
-		-- vim.api.nvim_buf_create_user_command(buf, 'ClangdSwitchSourceHeader', function()
-		--   switch_source_header(client, buf)
-		-- end, {
-		--   bar = true,
-		--   desc = 'clangd: Switch Between Source and Header File',
-		-- })
-		-- vim.keymap.set('n', 'grs', '<Cmd>ClangdSwitchSourceHeader<CR>', {
-		--   buffer = buf,
-		--   desc = 'clangd: Switch Between Source and Header File',
-		-- })
-		--
-		-- vim.api.nvim_create_autocmd('LspDetach', {
-		--   group = vim.api.nvim_create_augroup('conf_lsp_attach_detach', { clear = false }),
-		--   buffer = buf,
-		--   callback = function(args)
-		--     if args.data.client_id == client.id then
-		--       vim.keymap.del('n', 'grs', { buffer = buf })
-		--       vim.api.nvim_buf_del_user_command(buf, 'ClangdSwitchSourceHeader')
-		--       return true -- Delete this autocmd.
-		--     end
-		--   end,
-		-- })
+	on_attach = function(client, buf)
+		vim.api.nvim_buf_create_user_command(
+			buf,
+			'ClangdSwitchSourceHeader',
+			function() switch_source_header(client, buf) end,
+			{
+				bar = true,
+				desc = 'clangd: Switch Between Source and Header File',
+			}
+		)
+		vim.keymap.set('n', 'grs', '<Cmd>ClangdSwitchSourceHeader<CR>', {
+			buffer = buf,
+			desc = 'clangd: Switch Between Source and Header File',
+		})
+
+		vim.api.nvim_create_autocmd('LspDetach', {
+			group = vim.api.nvim_create_augroup('conf_lsp_attach_detach', { clear = false }),
+			buffer = buf,
+			callback = function(args)
+				if args.data.client_id == client.id then
+					vim.keymap.del('n', 'grs', { buffer = buf })
+					vim.api.nvim_buf_del_user_command(buf, 'ClangdSwitchSourceHeader')
+					return true -- Delete this autocmd.
+				end
+			end,
+		})
 	end,
 } --[[@as vim.lsp.Config]]
