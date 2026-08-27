f351582dc439bc7bae3f6975a261ae235bc2a7c3
Petr Kadlec <xexperimente@gmail.com>
2026-08-27 21:37:25 +0200

    [Neovim] switch to blink.cmp v2

--- nvim/plugin/blink.cmp.lua
+++ nvim/plugin/blink.cmp.lua
@@ -1,54 +1,100 @@
-vim.schedule(function()
-	vim.pack.add({ { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('1.*') } })
+vim.pack.add({
+	'https://github.com/saghen/blink.lib',
+	'https://github.com/saghen/blink.cmp',
+})
 
-	local opts = {
-		fuzzy = {
-			implementation = 'prefer_rust_with_warning',
-		},
+local opts = {
+	fuzzy = { implementation = 'rust' },
+	keymap = {
+		preset = 'super-tab',
+		['<C-n>'] = { 'show', 'select_next', 'fallback_to_mappings' },
+		['<C-d>'] = { 'show_documentation', 'hide_documentation' },
+	},
+	sources = { default = { 'lsp', 'path', 'snippets', 'buffer' }, min_keyword_length = 2 },
+	signature = { enabled = true, window = { show_documentation = false } },
+	cmdline = {
+		enabled = true,
 		keymap = {
 			preset = 'super-tab',
 			['<C-n>'] = { 'show', 'select_next', 'fallback_to_mappings' },
-			['<C-d>'] = { 'show_documentation', 'hide_documentation' },
-		},
-		sources = { default = { 'lsp', 'path', 'snippets' }, min_keyword_length = 2 },
-		signature = { enabled = true, window = { show_documentation = false } },
-		cmdline = {
-			enabled = true,
-			keymap = {
-				preset = 'super-tab',
-				['<C-n>'] = { 'show', 'select_next', 'fallback_to_mappings' },
-				['<C-p>'] = { 'show', 'select_prev', 'fallback_to_mappings' },
-			},
-			completion = {
-				menu = {
-					auto_show = true,
-					draw = { columns = { { 'label', 'label_description', gap = 1 } } },
-				},
-			},
+			['<C-p>'] = { 'show', 'select_prev', 'fallback_to_mappings' },
 		},
 		completion = {
-			list = { selection = { auto_insert = false, preselect = true } },
-			trigger = {
-				show_on_keyword = true,
-				show_on_trigger_character = true,
-				show_on_insert_on_trigger_character = true,
-				show_on_accept_on_trigger_character = true,
-			},
 			menu = {
 				auto_show = true,
-				border = vim.g.winborder,
-				draw = {
-					treesitter = { 'lsp' },
-					columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'kind' } },
-				},
-			},
-			documentation = {
-				auto_show = false,
-				auto_show_delay_ms = 500,
-				window = { border = vim.g.winborder },
+				draw = { columns = { { 'label', 'label_description', gap = 1 } } },
 			},
 		},
-	}
+	},
+	completion = {
+		list = { selection = { auto_insert = false } },
+		menu = {
+			auto_show = true,
+			draw = {
+				treesitter = { 'lsp' },
+				columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'kind' } },
+			},
+		},
+		documentation = { auto_show = false },
+	},
+}
 
-	require('blink.cmp').setup(opts --[[@as blink.cmp.Config]])
-end)
+vim.defer_fn(function()
+	require('blink.cmp').build():pwait()
+	require('blink.cmp').setup(opts)
+end, 0)
+
+-- vim.schedule(function()
+-- 	vim.pack.add({ { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('1.*') } })
+--
+-- 	local opts = {
+-- 		fuzzy = {
+-- 			implementation = 'prefer_rust_with_warning',
+-- 		},
+-- 		keymap = {
+-- 			preset = 'super-tab',
+-- 			['<C-n>'] = { 'show', 'select_next', 'fallback_to_mappings' },
+-- 			['<C-d>'] = { 'show_documentation', 'hide_documentation' },
+-- 		},
+-- 		sources = { default = { 'lsp', 'path', 'snippets' }, min_keyword_length = 2 },
+-- 		signature = { enabled = true, window = { show_documentation = false } },
+-- 		cmdline = {
+-- 			enabled = true,
+-- 			keymap = {
+-- 				preset = 'super-tab',
+-- 				['<C-n>'] = { 'show', 'select_next', 'fallback_to_mappings' },
+-- 				['<C-p>'] = { 'show', 'select_prev', 'fallback_to_mappings' },
+-- 			},
+-- 			completion = {
+-- 				menu = {
+-- 					auto_show = true,
+-- 					draw = { columns = { { 'label', 'label_description', gap = 1 } } },
+-- 				},
+-- 			},
+-- 		},
+-- 		completion = {
+-- 			list = { selection = { auto_insert = false, preselect = true } },
+-- 			trigger = {
+-- 				show_on_keyword = true,
+-- 				show_on_trigger_character = true,
+-- 				show_on_insert_on_trigger_character = true,
+-- 				show_on_accept_on_trigger_character = true,
+-- 			},
+-- 			menu = {
+-- 				auto_show = true,
+-- 				border = vim.g.winborder,
+-- 				draw = {
+-- 					treesitter = { 'lsp' },
+-- 					columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'kind' } },
+-- 				},
+-- 			},
+-- 			documentation = {
+-- 				auto_show = false,
+-- 				auto_show_delay_ms = 500,
+-- 				window = { border = vim.g.winborder },
+-- 			},
+-- 		},
+-- 	}
+--
+-- 	require('blink.cmp').setup(opts --[[@as blink.cmp.Config]])
+-- end)
